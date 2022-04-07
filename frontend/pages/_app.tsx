8245fde3816d330bfcd950351443ee102dbdefc2
Mike Alicea <mfalicea58@gmail.com>
2022-04-07 14:05:41 -0500

    Remove the unecessary

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Communion</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
-export default MyApp
+export default MyApp;
