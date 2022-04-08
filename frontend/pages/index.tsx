d5d99d9333c521bab4b9e40e687d7ba5e7f8321c
Mike Alicea <mfalicea58@gmail.com>
2022-04-08 13:28:01 -0500

    Show balance,

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import type { Near, WalletConnection } from "near-api-js";
+import type { AccountBalance } from "near-api-js/lib/account";
 
 import getNearConfig from "../pages_util/near-api-util/near-config";
 import initNearWallet, {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
   const router = useRouter();
   const [near, setNear] = useState<Near>();
   const [wallet, setWallet] = useState<WalletConnection>();
+  const [walletBalance, setWalletBalance] = useState<AccountBalance>();
 
   const initNear = async (): Promise<void> => {
     const nearConfig = getNearConfig("dev");
@@ -22,6 +24,8 @@ const Home: NextPage = () => {
       setNear(rawNear.near);
       if (rawNear.wallet.isSignedIn()) {
         setWallet(rawNear.wallet);
+        console.log(await rawNear.wallet.account().getAccountDetails());
+        console.log(await rawNear.wallet.account().getAccountBalance());
       }
       console.log("initNear", rawNear);
     }
@@ -43,26 +47,38 @@ const Home: NextPage = () => {
   };
 
   useEffect(() => {
-    initNear();
-  }, []);
+    if (!near) {
+      initNear();
+    }
+  }, [near]);
+
+  useEffect(() => {
+    console.log("fire once");
+    const getWalletBalance = async (): Promise<void> => {
+      if (wallet) {
+        const balance = await wallet.account().getAccountBalance();
+        setWalletBalance(balance);
+      }
+    };
+    getWalletBalance();
+  }, [wallet]);
 
   return (
-    <div className="flex flex-col">
-      <main className="p-5">
-        <h1 className="text-center text-3xl">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <h1 className="text-center text-3xl font-bold">
-          Hello Tailwind, you so cute 🧡!
-        </h1>
+    <div className="">
+      <main>
+        <div className="flex flex-col place-items-center p-5">
+          <h1 className="text-3xl">Welcome to some NEAR goodness</h1>
 
-        {!wallet && <button onClick={connectWallet}>Connect Wallet</button>}
-        {wallet && (
-          <div>
-            <p>{wallet.getAccountId()}</p>
-            <button onClick={signOutOfWallet}>Sign Out of Wallet</button>
-          </div>
-        )}
+          {wallet ? (
+            <>
+              <p>{wallet.getAccountId()}</p>
+              <button onClick={signOutOfWallet}>Sign Out of Wallet</button>
+              <p>Balance: {walletBalance?.available}</p>
+            </>
+          ) : (
+            <button onClick={connectWallet}>Connect Wallet</button>
+          )}
+        </div>
       </main>
     </div>
   );
